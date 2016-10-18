49e9c48030728f03a32f1163b63d9d81e01002a3
Wessel Bosman <wessel.bosman@outlook.com>
2016-10-18 20:43:00 +0200

    - Initial commit - Created initial unit tests for calculating factorials

---
+++ nFactorial/Factorial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nFactorial
+{
+	public class Factorial
+	{
+		public static long Calculate(long n )
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ nFactorialTests/FactorialTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nFactorial;
+
+namespace nFactorialTests
+{
+	[TestClass]
+	public class FactorialTests
+	{
+		[TestMethod]
+		public void NumberLessThanOneFactorialIsOne()
+		{
+			//Arrange
+			var NumberUnderTestZero = 0;
+			var NumberUnderTestOne = 0;
+
+			//Act
+			var resultZero = Factorial.Calculate(NumberUnderTestZero);
+			var resultOne = Factorial.Calculate(NumberUnderTestOne);
+
+			//Assert
+			Assert.AreEqual(1,resultZero,"The factorial for 0 is not 1");
+			Assert.AreEqual(1, resultOne, "The factorial for 1 is not 1");
+
+		}
+	}
+}
